61d99d872aad5ea54ef74477248b0a7a1466f178
Ayush0115D <dhakreayush578@gmail.com>
2026-03-30 16:27:01 +0530

    second largest element

---
+++ Array3.java
@@ -0,0 +1,19 @@
+public class Array3 {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 8};
+
+        int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
+            }
+        }
+
+        System.out.println("Second Largest: " + second);
+    }
+}  
+
