95671b9539d12655854ae2f603fe37b1d7247ca8
Ayush0115D <dhakreayush578@gmail.com>
2026-04-03 23:10:03 +0530

    find missing number

---
+++ Array6.java
@@ -0,0 +1,15 @@
+public class Array6 {
+   
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5};
+        int n = 5;
+
+        int sum = n * (n + 1) / 2;
+
+        for (int num : arr)
+            sum -= num;
+
+        System.out.println("Missing: " + sum);
+    }
+}
+
