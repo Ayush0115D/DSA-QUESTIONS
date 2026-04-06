fc7d8f16bb67a3e04940b664b8aae32686ed2ce8
Ayush0115D <dhakreayush578@gmail.com>
2026-04-06 23:32:47 +0530

    max subarray sum

---
+++ Array9.java
@@ -0,0 +1,16 @@
+public class Array9 {
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int maxSum = arr[0], current = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            current = Math.max(arr[i], current + arr[i]);
+            maxSum = Math.max(maxSum, current);
+        }
+
+        System.out.println("Max Subarray Sum: " + maxSum);
+    }
+}  
+
