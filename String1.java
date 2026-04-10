8b6d65d5e9c943c3ca117b48f345a20f2c64932b
Ayush0115D <dhakreayush578@gmail.com>
2026-04-10 21:35:13 +0530

    rev a string

---
+++ String1.java
@@ -0,0 +1,13 @@
+public class String1 {
+   
+    public static void main(String[] args) {
+        String str = "Ayush";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        System.out.println("Reversed: " + reversed);
+    }
+}
