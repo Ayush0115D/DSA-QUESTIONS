249f7ef5ac30006feb705d3a4ba7240601eaf10c
Ayush0115D <dhakreayush578@gmail.com>
2026-04-12 01:09:21 +0530

    word count in a string

---
+++ String4.java
@@ -0,0 +1,8 @@
+public class String4 {
+    public static void main(String[] args) {
+        String str = "Java is awesome";
+        String[] words = str.split(" ");
+
+        System.out.println("Word Count: " + words.length);
+    }
+}  
