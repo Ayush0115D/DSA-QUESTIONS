e706cd5851eda4b1c9b426addaa3f17bdc6def66
Ayush0115D <dhakreayush578@gmail.com>
2026-07-18 12:16:09 +0530

    abstraction and interfaces

--- oops2.java
+++ oops2.java
@@ -1,29 +1,49 @@
+// abstract class Animal{
+//    abstract void walk();
+// }
+// class Horse extends Animal{
+//    void walk(){
+//       System.out.println("walks on 4 legs");
+//    }
+// }
+// class Chicken extends Animal{
+//    void walk(){
+//       System.out.println("walks on 2 legs");
+//    }
+// }
+
+// public class oops2 {
+//    public static void main(String[] args) {
+//      Horse horse=new Horse();
+//      horse.walk(); 
+//    //   Animal animal=new Animal(); // This line will give error because we cannot create object of abstract class
+//    //   animal.walk();              //animal ko hum bna hi ni sakte as its abstract concept   
+//    }
+// }
+interface Animal{
+   void walk();
+   // Animal(){ // This line will give error because we cannot create constructor of interface
+   // }
+   // void eat(){ // This line will give error because we cannot create method with body in interface,we can do in subclass
+   // }
+}
+interface herbivore{
+   void eatGrass();
+}
+class Horse implements Animal, herbivore{ 
+   public void walk(){
+      System.out.println("walks on 4 legs");
+   }
+   public void eatGrass(){
+      System.out.println("eats grass");
+   }
+}
+
 public class oops2 {
-   
-// abstraction
-   abstract class Shape {
-      abstract void draw();
-   }
-
-   // inheritance
-   class Circle extends Shape {
-      void draw() {
-         System.out.println("Drawing Circle");
-      }
-   }
-
-   class Rectangle extends Shape {
-      void draw() {
-         System.out.println("Drawing Rectangle");
-      }
-   }
-
    public static void main(String[] args) {
-      oops2 obj = new oops2();
-      Shape circle = obj.new Circle();
-      Shape rectangle = obj.new Rectangle();
-
-      circle.draw();
-      rectangle.draw();
+     Horse horse=new Horse();
+     horse.walk(); 
+   //   Animal animal=new Animal(); // This line will give error because we cannot create object of interface
+   //   animal.walk();              //animal ko hum bna hi ni sakte as its abstract concept   
    }
 }
